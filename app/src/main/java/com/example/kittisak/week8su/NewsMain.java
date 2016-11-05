ff23dc900eb6291c78797b277f4c22264540eca0
Kittisak Bouhom <Kittisak Bouhom>
2016-11-05 11:36:46 +0700

    first commit

---
+++ app/src/main/java/com/example/kittisak/week8su/NewsMain.java
@@ -0,0 +1,13 @@
+package com.example.kittisak.week8su;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class NewsMain extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_news_main);
+    }
+}
